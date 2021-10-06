b2a8dcf0554780eb14bf1092ba4957bf4f2fc29b
Tebenkoff <91970617+Tebenkoff@users.noreply.github.com>
2021-10-06 12:05:46 +0300

    Update test1.c

--- test1.c
+++ test1.c
@@ -6,7 +6,7 @@
 #define p_max 780  // max davlenie
 #define p_norm 760 // normalnoe davlenie
 
-int pressure[31]; //31 den v mesyatse
+int pressure[days]; //31 den v mesyatse
 
 int main()
 {
@@ -15,14 +15,21 @@ int main()
   for(int i=0; i<days; i++)
     printf("%d\t%d", i+1, pressure[i])
     
-  int cnt_blow = 0;
+  int cnt_below = 0;
   int cnt_above = 0;
   int cnt_exact = 0;
   
   for(int i=0; i<days; i++)
   {
     int v = pressure[i];
-    if(v < p_norm) cnt_bellow++;
-    else if (v )
+    if(v < p_norm) cnt_below++;
+    else if (v > p_norm) cnt_above++;
+    else cnt_exact++;
   }
+  
+  printf("below: %d\n", cnt_below);
+  printf("above: %d\n", cnt_above);
+  printf("exact: %d\n", cnt_exact);
+  
+  return 0;
 }
