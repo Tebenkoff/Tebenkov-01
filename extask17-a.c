59ba26497931596d348d819c184a59095ac6e195
Tebenkoff <91970617+Tebenkoff@users.noreply.github.com>
2021-10-20 12:23:58 +0300

    Update extask17-a.c

--- extask17-a.c
+++ extask17-a.c
@@ -4,7 +4,7 @@
 int main()
 {
   char s[256];
-  fgets(s, 256, stdin);  \\ Vvod s klaviatury
+  fgets(s, 256, stdin);  // Vvod s klaviatury
     
   size_t n = strlen(s);
   printf("len: %ld\n", n);
