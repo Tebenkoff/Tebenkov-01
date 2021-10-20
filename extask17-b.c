a41c3782086d7954ae53e7cede9eda857e5c84b9
Tebenkoff <91970617+Tebenkoff@users.noreply.github.com>
2021-10-20 12:44:45 +0300

    Update extask17-b.c

--- extask17-b.c
+++ extask17-b.c
@@ -1,1 +1,14 @@
+#include <stdio.h>
+#include <string.h>
 
+int main()
+{
+  char s[256];
+  fgets(s, 256, stdin);  // Vvod s klaviatury
+    
+  size_t n = strlen(s);
+  printf("len: %ld\n", n); // Vyvodit kol-vo simvolov vmeste s enterom
+  // esli nado bez enterov, togda printf("len: %ld\n", n - 1);
+  
+  return 0;
+}
