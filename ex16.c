e0d66d8e7e3fdbfd3904d078af6e35989bcadf2d
Tebenkoff <91970617+Tebenkoff@users.noreply.github.com>
2021-10-20 11:33:13 +0300

    Update ex16.c

--- ex16.c
+++ ex16.c
@@ -2,13 +2,29 @@
 
 struct record
 {
-  char drug_name[64];
-  char inducations[64];
-  int exp_years;
-  int mfg_year;
+  char drug_name[64];      // Nazvanie lekarstva
+  char indications[64];    // Pokazaniya k primeneniu
+  int exp_years;           // Srok godnosti
+  int mfg_year;            // God proizvodstva
 };
+
+#define n 3
+
+struct record row[n];
 
 int main()
 {
+  for (int i=0; i<n; i++)
+  {
+    scanf("%s %s %d %d", row[i].drug_name , row[i].indications, &row[i].exp_years, &row[i].mfg_year);
+  }
   
+  printf("name\tindics\texp\tyear\n");
+  
+  for (int i=0; i<n; i++)
+  {
+    printf("%s\t%s\t%d\t%d\n", row[i].drug_name , row[i].indications, row[i].exp_years, row[i].mfg_year);
+  }
+  
+  return 0;
 }
