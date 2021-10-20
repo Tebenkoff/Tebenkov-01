af1732e578eb3e6cf86869e2305b764590b5100e
Tebenkoff <91970617+Tebenkoff@users.noreply.github.com>
2021-10-20 12:44:20 +0300

    Update extask17-a.c

--- extask17-a.c
+++ extask17-a.c
@@ -8,7 +8,7 @@ int main()
     
   size_t n = strlen(s);
   printf("len: %ld\n", n); // Vyvodit kol-vo simvolov vmeste s enterom
-  // esli nado bez, togda printf("len: %ld\n", n - 1);
+  // esli nado bez enterov, togda printf("len: %ld\n", n - 1);
   
   return 0;
 }
